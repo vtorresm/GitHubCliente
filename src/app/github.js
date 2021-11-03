c56eb1a490f31da9d547af9ef0f5b471910fd09c
Victor Alberto Torres Mori <vtorremo@everis.com>
2021-11-02 20:39:38 -0500

    Actualizando proyecto

--- src/app/github.js
+++ src/app/github.js
@@ -1,32 +1,31 @@
 class Github {
-
-    //Pasamos parametros de client Id y client Secret
-    constructor(clientId, clientSecret) {
-        if(!clientId || !clientSecret) {
-            return console.warn('Please use a client_id and a client_secret');
-        }
-        this.client_id = clientId
-        this.client_secret = clientSecret
-        this.repos_count = 10
-        this.repos_sort = 'created: asc'
+  //Pasamos parametros de client Id y client Secret
+  constructor(clientId, clientSecret) {
+    if (!clientId || !clientSecret) {
+      return console.warn('Please use a client_id and a client_secret');
     }
+    this.client_id = clientId;
+    this.client_secret = clientSecret;
+    this.repos_count = 10;
+    this.repos_sort = 'created: asc';
+  }
 
-    async fetchUser(user) {
-        const userDataRequest = 
-                    await fetch
-                    (`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
-        const repositoriesRequest =            
-                    await fetch
-                        (`http://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}&per_page=${this.repos_count}&sort=${this.repos_sort}`)       
-        const userData = await userDataRequest.json()
-        const repositories = await repositoriesRequest.json()   
-                    // console.userData = userDataRequest.json
-                    // console.log(userDataRequest)
-        return {
-            userData,
-            repositories
-        }   
-    }
+  async fetchUser(user) {
+    const userDataRequest = await fetch(
+      `http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+    const repositoriesRequest = await fetch(
+      `http://api.github.com/users/${user}/repos?client_id=${this.client_id}&client_secret=${this.client_secret}&per_page=${this.repos_count}&sort=${this.repos_sort}`
+    );
+    const userData = await userDataRequest.json();
+    const repositories = await repositoriesRequest.json();
+    // console.userData = userDataRequest.json
+    // console.log(userDataRequest)
+    return {
+      userData,
+      repositories,
+    };
+  }
 }
 
-module.exports = Github
+module.exports = Github;
--- src/app/ui.js
+++ src/app/ui.js
@@ -1,54 +1,54 @@
 class UI {
-    constructor() {
-        this.profile = document.getElementById('profile')
-    }
+  constructor() {
+    this.profile = document.getElementById('profile');
+  }
 
-    showProfile(user) {
-        this.profile.innerHTML = `
-            <div class="card mt-2 animated bounceInLeft">
-               <img src="${user.avatar_url}" class="card-img-top"/>
-               <div class="card-body">
-                    <h3 class="card-title">${user.name} / ${user.login}</h3>
-                    <a href="${user.html_url}" class="btn btn-primary btn-block" target="_blank">View Profile</a>
-                    <span class="badge badge-success">
-                        Followers: ${user.follower}
-                    </span>
-                    <span class="badge badge-primary">
-                        Followers: ${user.following}
-                    </span>
-                    <span class="badge badge-warning">
-                        Company: ${user.company}
-                    </span>
-                    <span class="badge badge-info d-block">
-                        Blog: ${user.blog}
-                    </span>
-               </div>
-            </div>
-        `
-        this.clearMessage()
-    }
+  showProfile(user) {
+    this.profile.innerHTML = `
+    <div class="card mt-2 animated bounceInLeft">
+        <img src="${user.avatar_url}" class="card-img-top"/>
+        <div class="card-body">
+            <h3 class="card-title">${user.name} / ${user.login}</h3>
+            <a href="${user.html_url}" class="btn btn-primary btn-block" target="_blank">View Profile</a>
+            <span class="badge badge-success">
+                Followers: ${user.follower}
+            </span>
+            <span class="badge badge-primary">
+                Followers: ${user.following}
+            </span>
+            <span class="badge badge-warning">
+                Company: ${user.company}
+            </span>
+            <span class="badge badge-info d-block">
+                Blog: ${user.blog}
+            </span>
+        </div>
+    </div>
+        `;
+    this.clearMessage();
+  }
 
-    showMessage(message, cssClass) {
-        const div = document.createElement('div')
-        div.className = cssClass
-        div.appendChild(document.createTextNode(message))
-        const content = document.querySelector('.row')
-        const profile = document.querySelector('#profile')
-        content.insertBefore(div, profile)
-        setTimeout(() => this.clearMessage(), 3000)
-    }
+  showMessage(message, cssClass) {
+    const div = document.createElement('div');
+    div.className = cssClass;
+    div.appendChild(document.createTextNode(message));
+    const content = document.querySelector('.row');
+    const profile = document.querySelector('#profile');
+    content.insertBefore(div, profile);
+    setTimeout(() => this.clearMessage(), 3000);
+  }
 
-    clearMessage() {
-        const alertFound = document.querySelector('.alert')
-        if(alertFound) {
-            alertFound.remove()
-        }
+  clearMessage() {
+    const alertFound = document.querySelector('.alert');
+    if (alertFound) {
+      alertFound.remove();
     }
+  }
 
-    showRepositories(repositories) {
-        let template = ''
-        repositories.forEach(repo => {
-            template += `
+  showRepositories(repositories) {
+    let template = '';
+    repositories.forEach((repo) => {
+      template += `
             <div class="card card card-body mt-2 animated bounceInUp">
                 <div class="row">
                     <div class="col-md-6">
@@ -61,21 +61,21 @@ class UI {
                         <span class="badge badge-success">
                             forks: ${repo.forks_count}
                         </span>
-                    </div>     
+                    </div>
                 </div>
             </div>
-        `
-        });
-        document.getElementById('repositories').innerHTML = template
-    }
+        `;
+    });
+    document.getElementById('repositories').innerHTML = template;
+  }
 
-    reset() {
-        this.profile.innerHTML = `
+  reset() {
+    this.profile.innerHTML = `
           <div class="container mt5">
             <h3 class="display-2 text-center">Learn Something New Everydary</h3>
           </div>
-        `
-    }
+        `;
+  }
 }
 
-module.exports = UI
+module.exports = UI;
